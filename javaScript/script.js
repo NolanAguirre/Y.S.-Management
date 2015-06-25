3370263057124c384face284a1a74d7f08591c0c
Nolan Aguirre <bobby7083@gmail.com>
2015-06-24 23:23:28 -0500

    clean up code, remove some unused styling, make home pictures mobile friendly

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,55 +1,56 @@
-angular.module('app',[]).
-    controller('myController', apartmentController);
-    //TODO:
-    //Add map
-    //Add gallery 
-    //Add online issue report
-    //Finish HTML
-    function apartmentController(){
-        var vm = this;
-        function slide(type, template){
-            this.type = type;
-            this.template = template
-        }
-        function homeSlide(image, title){
-            this.image = image;
-            this.title = title;
-        };
-        slide.prototype.isCurrentSlide = function() {
-            return this === vm.currentSlide;
-        };
+angular.module('app', []).
+controller('myController', apartmentController);
+//TODO:
+//Fix image resize
+//Fix text resize
+//Make code more mobile friendly
+//Finish HTML
+function apartmentController() {
+    var vm = this;
 
-        slide.prototype.show = function(){
-            vm.currentSlide = this;
-        };
-        vm.slides = [
-            new slide('Home','Html/home.html'),
-            new slide('Floor Plan','Html/floorPlan.html'),
-            new slide('Gallery',"Html/gallery.html"),
-            new slide('Residents', 'Html/residents.html'),
-            new slide('Application','Html/rentailApplication.html'),
-            new slide('Contact us','Html/contactUs.html'),
-            new slide('About us','Html/aboutUs.html'),
-            new slide('Map','Html/map.html')
-    ];
-        vm.currentSlide = vm.slides[0];
-        vm.slides[1].plans = [{
-            image:'photos/demoOne.jpg',
-            description:'Two Story, Storage unit optional. Two Bedroom,, One and a half Bath',
-            sqft:'1127 Sq. Ft.',
-            price:'725$ /Monthly'
-            },{
-            image:'photos/demoTwo.jpg',
-            description:'Two Story, Storage unit optional. Two Bedroom,, One and a half Bath',
-            sqft:'1327 Sq. Ft.',
-            price:'750$ /Monthly'         
-        }]
-        vm.homeSlides = [
-            new homeSlide('demoOne.jpg','View More!'),
-            new homeSlide('demoTwo.jpg','View Map!'),
-            new homeSlide('demoThree.jpg','Learn More!')        
-        ]
-        
-       
+    function slide(type, template) {
+        this.type = type;
+        this.template = template
     }
- 
+
+    function homeSlide(image, title) {
+        this.image = image;
+        this.title = title;
+    };
+    slide.prototype.isCurrentSlide = function() {
+        return this === vm.currentSlide;
+    };
+
+    slide.prototype.show = function() {
+        vm.currentSlide = this;
+    };
+    vm.slides = [
+        new slide('Home', 'Html/home.html'),
+        new slide('Floor Plan', 'Html/floorPlan.html'),
+        new slide('Gallery', "Html/gallery.html"),
+        new slide('Residents', 'Html/residents.html'),
+        new slide('Application', 'Html/rentailApplication.html'),
+        new slide('Contact us', 'Html/contactUs.html'),
+        new slide('About us', 'Html/aboutUs.html'),
+        new slide('Map', 'Html/map.html')
+    ];
+    vm.currentSlide = vm.slides[0];
+    vm.slides[1].plans = [{
+        image: 'photos/demoOne.jpg',
+        description: 'Two Story, Storage unit optional. Two Bedroom,, One and a half Bath',
+        sqft: '1127 Sq. Ft.',
+        price: '725$ /Monthly'
+    }, {
+        image: 'photos/demoTwo.jpg',
+        description: 'Two Story, Storage unit optional. Two Bedroom,, One and a half Bath',
+        sqft: '1327 Sq. Ft.',
+        price: '750$ /Monthly'
+    }]
+    vm.homeSlides = [
+        new homeSlide('demoOne.jpg', 'View More!'),
+        new homeSlide('demoTwo.jpg', 'View Map!'),
+        new homeSlide('demoThree.jpg', 'Learn More!')
+    ]
+
+
+}
