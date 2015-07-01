e506aa4d56df01acdecfdb521c9690e4aef7e4c6
Tenor Biel <tenorbiel@gmail.com>
2015-07-01 01:04:35 -0500

    add more routes and initial styles

--- js/router.js
+++ js/router.js
@@ -4,7 +4,12 @@ angular.module('ysm')
 Router.$inject = ['$routeProvider'];
 function Router($routeProvider) {
     $routeProvider
-        .when('/', {
-            templateUrl: 'templates/home.html'
-        });
+        .when('/', {templateUrl: 'templates/home.html'})
+        .when('/gallery', {templateUrl: 'templates/gallery.html'})
+        .when('/about-us', {templateUrl: 'templates/about-us.html'})
+        .when('/residence/:letter', {
+            controller: 'ResidenceController as vm',
+            templateUrl: 'templates/residence.html'
+        })
+        .otherwise({redirectTo: '/'});
 }
