15fe2e0354106e005cb2ab5ab8be9b0db2a365df
Nolan Aguirre <bobby7083@gmail.com>
2015-07-04 11:12:03 -0500

    merge map and residents into other tabs

--- javaScript/script.js
+++ javaScript/script.js
@@ -25,22 +25,19 @@ function apartmentController() {
     vm.slides = [
         new slide('Home', 'Html/home.html'),
         new slide('Floor Plan', 'Html/floorPlan.html'),
-        new slide('Gallery', "Html/gallery.html"),
-        new slide('Residents', 'Html/residents.html'),
         new slide('Application', 'Html/rentailApplication.html'),
         new slide('Contact us', 'Html/contactUs.html'),
-        new slide('About us', 'Html/aboutUs.html'),
-        new slide('Map', 'Html/map.html')
+        new slide('About us', 'Html/aboutUs.html')
     ];
     vm.currentSlide = vm.slides[0];
     vm.slides[1].plans = [{
         image: 'photos/demoOne.jpg',
-        description: 'Two Story, Storage unit optional. Two Bedroom,, One and a half Bath',
+        description: 'Two Story, Storage unit optional. Two Bedroom, One and a half Bath',
         sqft: '1127 Sq. Ft.',
         price: '725$ /Monthly'
     }, {
         image: 'photos/demoTwo.jpg',
-        description: 'Two Story, Storage unit optional. Two Bedroom,, One and a half Bath',
+        description: 'Two Story, Storage unit optional. Two Bedroom, One and a half Bath',
         sqft: '1327 Sq. Ft.',
         price: '750$ /Monthly'
     }]
