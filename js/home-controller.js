824d9cd0d1721493379f9e33affc555abe5d3558
Nolan Aguirre <bobby7083@gmail.com>
2015-07-23 18:17:59 -0500

    add links to mini nav bar

--- js/home-controller.js
+++ js/home-controller.js
@@ -1,14 +1,20 @@
 angular.module('ysm')
     .controller('HomeController', HomeController);
 
-ResidenceController.$inject = ['$routeParams'];
+HomeController.$inject = ['$routeParams']
 function HomeController($routeParams) {
     var vm = this;
     vm.slides = [{
-        image:'http://placehold.it/200x200/'
+        image:'http://placehold.it/150x150/',
+        link: '#/about-us',
+        text:'View map'
     },{
-        image:'http://placehold.it/200x200/'
+        image:'http://placehold.it/150x150/',
+        link:'#/gallery',
+        text:'View more'
     },{
-        image:'http://placehold.it/200x200/'
+        image:'http://placehold.it/150x150/',
+        link:'#/contact-us',
+        text:'View contact'
     }]
 }
